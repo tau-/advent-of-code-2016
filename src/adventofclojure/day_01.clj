db7bc544d6560fc874eb43e62ffb8c95775884cc
Alex Troesch <alex.troesch@meltwater.com>
2016-12-06 16:23:47 -0800

    Add day_01 part 2 solution in clojure

--- src/adventofclojure/day_01.clj
+++ src/adventofclojure/day_01.clj
@@ -4,24 +4,35 @@
 (def starting-state
   {:dir :n, :n 0, :s 0, :e 0, :w 0})
 
+(def starting-state-with-crumbs
+  (assoc starting-state :crumbs '({:x 0, :y 0})))
+
+(defn get-position
+  [state]
+  {:x (- (:n state) (:s state)), :y (- (:e state) (:w state))})
+
+(defn taxicab-distance
+  [state]
+  (let [position (get-position state)
+        x (:x position) y (:y position)]
+    (+ (Math/abs x) (Math/abs y))))
+
 (defn turn
   [state command]
-  (let [turn (first command) current-direction (:dir state)
-        left-turn
-        {:n :w, :s :e, :e :n, :w :s}
-        right-turn
-        {:n :e, :s :w, :e :s, :w :n}]
+  (let [turn (first command)
+        current-direction (:dir state)
+        left-turn {:n :w, :s :e, :e :n, :w :s}
+        right-turn {:n :e, :s :w, :e :s, :w :n}]
     (case turn
       \L (assoc state :dir (current-direction left-turn))
       \R (assoc state :dir (current-direction right-turn))
+      state
       )))
 
 (defn move
   [state command]
-  (let [distance
-        (Integer/parseInt (apply str (rest command)))
-        current-direction
-        (:dir state)]
+  (let [distance (Integer/parseInt (apply str (rest command)))
+        current-direction (:dir state)]
     (assoc state current-direction (+ (current-direction state) distance))
     ))
 
@@ -29,16 +40,34 @@
   [state command]
   (move (turn state command) command))
 
-(defn taxicab-distance
+(defn add-crumb
   [state]
-  (let [n (:n state) s (:s state) e (:e state) w (:w state)]
-    (+ (Math/abs (- n s)) (Math/abs (- e w)))
-    ))
+  (let [current-crumbs (:crumbs state)]
+    (assoc state :crumbs (cons (get-position state) current-crumbs))))
+
+(defn revisited?
+  [state]
+  (let [history (:crumbs state)]
+    (not= (count history) (count (set history)))))
+
+(defn move-with-crumbs
+  [state command]
+  (let [distance (Integer/parseInt (apply str (rest command)))]
+    (loop [n 0
+           s state]
+      (if (revisited? s)
+        (reduced s)
+        (if (< n distance)
+          (recur (inc n) (add-crumb (move s "X1")))
+          s)))))
+
+(defn turn-then-move-with-crumbs
+  [state command]
+  (move-with-crumbs (turn state command) command))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  (let [commands
-        (map str/trim-newline (str/split input #", "))]
+  (let [commands (map str/trim-newline (str/split input #", "))]
     [(taxicab-distance (reduce turn-then-move starting-state commands))
-    ]))
+     (taxicab-distance (reduce turn-then-move-with-crumbs starting-state-with-crumbs commands))]))
--- test/adventofclojure/day_01_test.clj
+++ test/adventofclojure/day_01_test.clj
@@ -2,26 +2,30 @@
   (:require [clojure.test :refer :all]
             [adventofclojure.day-01 :refer :all]))
 
-(deftest parse-directions-test
-  (is (=  [1 -1 1 1 1 -1] (parse-directions "()((()"))))
+(deftest move-test
+  (is (=  {:dir :n, :n 1, :s 0, :e 0, :w 0} (move starting-state "X1"))))
 
-(deftest steps-to-basement-test
-  (is (= 1 (steps-to-basement [-1])))
-  (is (= 9 (steps-to-basement [1 1 1 -1 1 -1 -1 -1 -1 -1 -1 1]))))
+(deftest turn-test
+  (is (=  {:dir :n, :n 0, :s 0, :e 0, :w 0} (turn starting-state "X0")))
+  (is (=  {:dir :w, :n 0, :s 0, :e 0, :w 0} (turn starting-state "L0")))
+  (is (=  {:dir :e, :n 0, :s 0, :e 0, :w 0} (turn starting-state "R0"))))
+
+(deftest add-crumb-test
+  (is (= {:dir :n, :n 1, :s 0, :e 1, :w 0, :crumbs '({:x 1, :y 1} {:x 0, :y 0})}
+         (add-crumb {:dir :n, :n 1, :s 0, :e 1, :w 0, :crumbs '({:x 0, :y 0})}))))
+
+(deftest revisited-test
+  (is (= true (revisited? {:dir :n, :n 1, :s 0, :e 1, :w 0, :crumbs '({:x 1, :y 1} {:x 1, :y 1})})))
+  (is (= false (revisited? {:dir :n, :n 1, :s 0, :e 1, :w 0, :crumbs '({:x 1, :y 1} {:x 0, :y 0})}))))
+
+(deftest taxicab-distance-test
+  (is (= 0 (taxicab-distance starting-state)))
+  (is (= 4 (taxicab-distance {:dir :n, :n 1, :s 3, :e 3, :w 1}))))
 
 (deftest solve-test
   (testing "part 1"
-    (is (=  0 (first (solve "(())"))))
-    (is (=  0 (first (solve "()()"))))
-    (is (=  3 (first (solve "((("))))
-    (is (=  3 (first (solve "(()(()("))))
-    (is (=  3 (first (solve "))((((("))))
-    (is (= -1 (first (solve "())"))))
-    (is (= -1 (first (solve "))("))))
-    (is (= -3 (first (solve ")))"))))
-    (is (= -3 (first (solve ")())())")))))
+    (is (=  5 (first (solve "R2, L3"))))
+    (is (=  2 (first (solve "R2, R2, R2"))))
+    (is (= 12 (first (solve "R5, L5, R5, R3")))))
   (testing "part 2"
-    (is (=  1 (second (solve ")"))))
-    (is (=  1 (second (solve "))"))))
-    (is (=  5 (second (solve "()())"))))
-    (is (= 11 (second (solve "(()()()()))))))(("))))))
+    (is (=  4 (second (solve "R8, R4, R4, R8"))))))
